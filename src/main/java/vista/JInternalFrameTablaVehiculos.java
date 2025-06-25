6633f4dca9cfcca5a84dc2c65ce9c21e6a36b88d
Amirtiva <amirtiva1412@gmail.com>
2025-06-25 08:13:15 -0500

    cambios para  aprobar a ultima hora

--- src/main/java/vista/JInternalFrameTablaVehiculos.java
+++ src/main/java/vista/JInternalFrameTablaVehiculos.java
@@ -4,8 +4,18 @@
  */
 package vista;
 
+import conexion.Conexion;
 import java.awt.Dimension;
-
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.*;
+import javax.swing.JTextField;
+import java.sql.*;
+import java.util.Vector;
 /**
  *
  * @author LENOVO
@@ -18,6 +28,10 @@ public class JInternalFrameTablaVehiculos extends javax.swing.JInternalFrame {
         this.setSize(new Dimension(894, 553));
         //this.setExtendedState(this.MAXIMIZED_BOTH);
         this.setTitle("Clientes");
+        this.setSize(new Dimension(894, 553));
+        
+        this.setTitle("Vehículos");
+        this.CargarTablaVehiculos();
     }
 
     /**
@@ -33,138 +47,141 @@ public class JInternalFrameTablaVehiculos extends javax.swing.JInternalFrame {
         jLabel1 = new javax.swing.JLabel();
         jPanel2 = new javax.swing.JPanel();
         jScrollPane1 = new javax.swing.JScrollPane();
-        jTableEmpleados = new javax.swing.JTable();
-        jPanel4 = new javax.swing.JPanel();
-        jButton1 = new javax.swing.JButton();
-        jButton2 = new javax.swing.JButton();
-        jPanel6 = new javax.swing.JPanel();
-        jLabel4 = new javax.swing.JLabel();
-        jTextField3 = new javax.swing.JTextField();
-        jLabel5 = new javax.swing.JLabel();
-        NumPlaca = new javax.swing.JTextField();
-        jLabel6 = new javax.swing.JLabel();
-        jLabel7 = new javax.swing.JLabel();
-        jTextField6 = new javax.swing.JTextField();
-        jLabel8 = new javax.swing.JLabel();
-        jLabel13 = new javax.swing.JLabel();
-        cbxColor = new javax.swing.JComboBox<>();
-        TipoVehiculo = new javax.swing.JComboBox<>();
-        LunasPolarizadasL = new javax.swing.JComboBox<>();
+        jTableVehiculos = new javax.swing.JTable(){@Override
+            public boolean isCellEditable(int row, int col) {
+                return false;  // nunca editable
+            }};
+            jPanel4 = new javax.swing.JPanel();
+            jButton1 = new javax.swing.JButton();
+            jButton2 = new javax.swing.JButton();
+            jPanel6 = new javax.swing.JPanel();
+            jLabel4 = new javax.swing.JLabel();
+            ModeloVehiculo = new javax.swing.JTextField();
+            jLabel5 = new javax.swing.JLabel();
+            NumPlacaVehiculos = new javax.swing.JTextField();
+            jLabel6 = new javax.swing.JLabel();
+            jLabel7 = new javax.swing.JLabel();
+            MarcaVehiculo = new javax.swing.JTextField();
+            jLabel8 = new javax.swing.JLabel();
+            jLabel13 = new javax.swing.JLabel();
+            ColorVehiculos = new javax.swing.JComboBox<>();
+            TipoVehiculo = new javax.swing.JComboBox<>();
+            LunasPolarizadasL = new javax.swing.JComboBox<>();
 
-        setClosable(true);
-        setIconifiable(true);
-        getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+            setClosable(true);
+            setIconifiable(true);
+            getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+            jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        jLabel1.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
-        jLabel1.setText("Administrar Vehiculos");
-        jPanel1.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(320, 10, -1, -1));
+            jLabel1.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
+            jLabel1.setText("Administrar Vehiculos");
+            jPanel1.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(320, 10, -1, -1));
 
-        jPanel2.setBackground(new java.awt.Color(255, 255, 255));
+            jPanel2.setBackground(new java.awt.Color(255, 255, 255));
 
-        jTableEmpleados.setModel(new javax.swing.table.DefaultTableModel(
-            new Object [][] {
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null}
-            },
-            new String [] {
-                "Title 1", "Title 2", "Title 3", "Title 4"
-            }
-        ));
-        jScrollPane1.setViewportView(jTableEmpleados);
+            jTableVehiculos.setModel(new javax.swing.table.DefaultTableModel(
+                new Object [][] {
+                    {null, null, null, null},
+                    {null, null, null, null},
+                    {null, null, null, null},
+                    {null, null, null, null}
+                },
+                new String [] {
+                    "Title 1", "Title 2", "Title 3", "Title 4"
+                }
+            ));
+            jScrollPane1.setViewportView(jTableVehiculos);
 
-        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
-        jPanel2.setLayout(jPanel2Layout);
-        jPanel2Layout.setHorizontalGroup(
-            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 710, Short.MAX_VALUE)
-        );
-        jPanel2Layout.setVerticalGroup(
-            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 250, Short.MAX_VALUE)
-        );
+            javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
+            jPanel2.setLayout(jPanel2Layout);
+            jPanel2Layout.setHorizontalGroup(
+                jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 710, Short.MAX_VALUE)
+            );
+            jPanel2Layout.setVerticalGroup(
+                jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 250, Short.MAX_VALUE)
+            );
 
-        jPanel1.add(jPanel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 60, 710, 250));
+            jPanel1.add(jPanel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 60, 710, 250));
 
-        jPanel4.setBackground(new java.awt.Color(255, 255, 255));
-        jPanel4.setBorder(javax.swing.BorderFactory.createEtchedBorder());
-        jPanel4.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+            jPanel4.setBackground(new java.awt.Color(255, 255, 255));
+            jPanel4.setBorder(javax.swing.BorderFactory.createEtchedBorder());
+            jPanel4.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        jButton1.setText("ACTUALIZAR");
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
-            }
-        });
-        jPanel4.add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 10, -1, -1));
+            jButton1.setText("ACTUALIZAR");
+            jButton1.addActionListener(new java.awt.event.ActionListener() {
+                public void actionPerformed(java.awt.event.ActionEvent evt) {
+                    jButton1ActionPerformed(evt);
+                }
+            });
+            jPanel4.add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 10, -1, -1));
 
-        jButton2.setText("ELIMINAR");
-        jPanel4.add(jButton2, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 50, 100, -1));
+            jButton2.setText("ELIMINAR");
+            jPanel4.add(jButton2, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 50, 100, -1));
 
-        jPanel1.add(jPanel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(730, 60, 140, 90));
+            jPanel1.add(jPanel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(730, 60, 140, 90));
 
-        jPanel6.setBackground(new java.awt.Color(255, 255, 255));
-        jPanel6.setBorder(javax.swing.BorderFactory.createEtchedBorder());
-        jPanel6.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+            jPanel6.setBackground(new java.awt.Color(255, 255, 255));
+            jPanel6.setBorder(javax.swing.BorderFactory.createEtchedBorder());
+            jPanel6.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        jLabel4.setFont(new java.awt.Font("Tahoma", 1, 12)); // NOI18N
-        jLabel4.setText("Modelo:");
-        jPanel6.add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(200, 60, -1, -1));
-        jPanel6.add(jTextField3, new org.netbeans.lib.awtextra.AbsoluteConstraints(200, 80, 180, -1));
+            jLabel4.setFont(new java.awt.Font("Tahoma", 1, 12)); // NOI18N
+            jLabel4.setText("Modelo:");
+            jPanel6.add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(200, 60, -1, -1));
+            jPanel6.add(ModeloVehiculo, new org.netbeans.lib.awtextra.AbsoluteConstraints(200, 80, 180, -1));
 
-        jLabel5.setFont(new java.awt.Font("Tahoma", 1, 12)); // NOI18N
-        jLabel5.setText("Nr de Placa:");
-        jPanel6.add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 10, -1, -1));
+            jLabel5.setFont(new java.awt.Font("Tahoma", 1, 12)); // NOI18N
+            jLabel5.setText("Nr de Placa:");
+            jPanel6.add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 10, -1, -1));
 
-        NumPlaca.addKeyListener(new java.awt.event.KeyAdapter() {
-            public void keyTyped(java.awt.event.KeyEvent evt) {
-                NumPlacaKeyTyped(evt);
-            }
-        });
-        jPanel6.add(NumPlaca, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 30, 180, -1));
+            NumPlacaVehiculos.addKeyListener(new java.awt.event.KeyAdapter() {
+                public void keyTyped(java.awt.event.KeyEvent evt) {
+                    NumPlacaVehiculosKeyTyped(evt);
+                }
+            });
+            jPanel6.add(NumPlacaVehiculos, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 30, 180, -1));
 
-        jLabel6.setFont(new java.awt.Font("Tahoma", 1, 12)); // NOI18N
-        jLabel6.setText("Color:");
-        jPanel6.add(jLabel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(200, 10, -1, -1));
+            jLabel6.setFont(new java.awt.Font("Tahoma", 1, 12)); // NOI18N
+            jLabel6.setText("Color:");
+            jPanel6.add(jLabel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(200, 10, -1, -1));
 
-        jLabel7.setFont(new java.awt.Font("Tahoma", 1, 12)); // NOI18N
-        jLabel7.setText("Tipo de Vehiculo:");
-        jPanel6.add(jLabel7, new org.netbeans.lib.awtextra.AbsoluteConstraints(390, 60, -1, -1));
-        jPanel6.add(jTextField6, new org.netbeans.lib.awtextra.AbsoluteConstraints(590, 30, 180, -1));
+            jLabel7.setFont(new java.awt.Font("Tahoma", 1, 12)); // NOI18N
+            jLabel7.setText("Tipo de Vehiculo:");
+            jPanel6.add(jLabel7, new org.netbeans.lib.awtextra.AbsoluteConstraints(390, 60, -1, -1));
+            jPanel6.add(MarcaVehiculo, new org.netbeans.lib.awtextra.AbsoluteConstraints(590, 30, 180, -1));
 
-        jLabel8.setFont(new java.awt.Font("Tahoma", 1, 12)); // NOI18N
-        jLabel8.setText("Tiene Lunas Polarizadas:");
-        jPanel6.add(jLabel8, new org.netbeans.lib.awtextra.AbsoluteConstraints(390, 10, -1, -1));
+            jLabel8.setFont(new java.awt.Font("Tahoma", 1, 12)); // NOI18N
+            jLabel8.setText("Tiene Lunas Polarizadas:");
+            jPanel6.add(jLabel8, new org.netbeans.lib.awtextra.AbsoluteConstraints(390, 10, -1, -1));
 
-        jLabel13.setFont(new java.awt.Font("Tahoma", 1, 12)); // NOI18N
-        jLabel13.setText("Marca:");
-        jPanel6.add(jLabel13, new org.netbeans.lib.awtextra.AbsoluteConstraints(590, 10, -1, -1));
+            jLabel13.setFont(new java.awt.Font("Tahoma", 1, 12)); // NOI18N
+            jLabel13.setText("Marca:");
+            jPanel6.add(jLabel13, new org.netbeans.lib.awtextra.AbsoluteConstraints(590, 10, -1, -1));
 
-        cbxColor.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "No Especificado", "Negro", "Gris", "Plata", "Azul", "Verde", "Rojo", "Amarillo", "Blanco", "Marrón", "Beige", "Celeste", "Turquesa", "Dorado", "Bronce", "Vino", "Anaranjado", "Lavanda", "Fucsia", "Lila", "Cian", "Magenta", "Púrpura", "Gris Oscuro", "Azul Marino", "Verde Oliva", "Negro Mate", "Blanco Perla" }));
-        jPanel6.add(cbxColor, new org.netbeans.lib.awtextra.AbsoluteConstraints(200, 30, 180, -1));
+            ColorVehiculos.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "No Especificado", "Negro", "Gris", "Plata", "Azul", "Verde", "Rojo", "Amarillo", "Blanco", "Marrón", "Beige", "Celeste", "Turquesa", "Dorado", "Bronce", "Vino", "Anaranjado", "Lavanda", "Fucsia", "Lila", "Cian", "Magenta", "Púrpura", "Gris Oscuro", "Azul Marino", "Verde Oliva", "Negro Mate", "Blanco Perla" }));
+            jPanel6.add(ColorVehiculos, new org.netbeans.lib.awtextra.AbsoluteConstraints(200, 30, 180, -1));
 
-        TipoVehiculo.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "No Especificado", "Sedan", "SUV", "Pickup", "Hatchback", "Convertible", "Coupé", "Furgoneta", "Camión", "Motocicleta", "Minivan" }));
-        jPanel6.add(TipoVehiculo, new org.netbeans.lib.awtextra.AbsoluteConstraints(390, 80, 180, -1));
+            TipoVehiculo.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "No Especificado", "Sedan", "SUV", "Pickup", "Hatchback", "Convertible", "Coupé", "Furgoneta", "Camión", "Motocicleta", "Minivan" }));
+            jPanel6.add(TipoVehiculo, new org.netbeans.lib.awtextra.AbsoluteConstraints(390, 80, 180, -1));
 
-        LunasPolarizadasL.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Si", "No" }));
-        jPanel6.add(LunasPolarizadasL, new org.netbeans.lib.awtextra.AbsoluteConstraints(390, 30, 180, -1));
+            LunasPolarizadasL.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Si", "No" }));
+            jPanel6.add(LunasPolarizadasL, new org.netbeans.lib.awtextra.AbsoluteConstraints(390, 30, 180, -1));
 
-        jPanel1.add(jPanel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 370, 860, 120));
+            jPanel1.add(jPanel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 370, 860, 120));
 
-        getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 890, 520));
+            getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 890, 520));
 
-        pack();
-    }// </editor-fold>//GEN-END:initComponents
+            pack();
+        }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_jButton1ActionPerformed
 
-    private void NumPlacaKeyTyped(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_NumPlacaKeyTyped
-String text = NumPlaca.getText(); // texto actual 
+    private void NumPlacaVehiculosKeyTyped(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_NumPlacaVehiculosKeyTyped
+String text = NumPlacaVehiculos.getText(); // texto actual 
     int lim = 10; // longitud máxima sugerida para placas
     if (text.length() >= lim) evt.consume();
 
@@ -175,14 +192,16 @@ String text = NumPlaca.getText(); // texto actual
         evt.consume();
     }
 
-    }//GEN-LAST:event_NumPlacaKeyTyped
+    }//GEN-LAST:event_NumPlacaVehiculosKeyTyped
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JComboBox<String> ColorVehiculos;
     private javax.swing.JComboBox<String> LunasPolarizadasL;
-    private javax.swing.JTextField NumPlaca;
+    private javax.swing.JTextField MarcaVehiculo;
+    private javax.swing.JTextField ModeloVehiculo;
+    private javax.swing.JTextField NumPlacaVehiculos;
     private javax.swing.JComboBox<String> TipoVehiculo;
-    private javax.swing.JComboBox<String> cbxColor;
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
     private javax.swing.JLabel jLabel1;
@@ -197,8 +216,43 @@ String text = NumPlaca.getText(); // texto actual
     private javax.swing.JPanel jPanel4;
     private javax.swing.JPanel jPanel6;
     private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JTable jTableEmpleados;
-    private javax.swing.JTextField jTextField3;
-    private javax.swing.JTextField jTextField6;
+    public static javax.swing.JTable jTableVehiculos;
     // End of variables declaration//GEN-END:variables
+private void CargarTablaVehiculos() {
+    DefaultTableModel model = new DefaultTableModel();
+    model.addColumn("NroPlaca");
+    model.addColumn("Color");
+    model.addColumn("Lunas_Polarizadas");
+    model.addColumn("Marca");
+    model.addColumn("Modelo");
+    model.addColumn("TipoVehiculo");
+
+    String sql = 
+        "SELECT V.NroPlaca, C.Nombre AS Color, V.Lunas_Polarizadas, " +
+        "       M.Nombre AS Marca, M.Nombre AS Modelo, T.Nombre AS TipoVehiculo " +
+        "FROM Vehiculos V " +
+        "JOIN Color C ON V.CodColor = C.CodColor " +
+        "JOIN Marca_Modelo M ON V.CodMarcaModelo = M.CodMarcaModelo " +
+        "JOIN Tipo_Vehiculo T ON V.CodTipoVhc = T.CodTipoVhc";
+
+    try (Connection cn = Conexion.establecerConexion();
+         Statement st = cn.createStatement();
+         ResultSet rs = st.executeQuery(sql)) {
+
+        while (rs.next()) {
+            Object[] fila = new Object[6];
+            for (int i = 0; i < fila.length; i++) {
+                fila[i] = rs.getObject(i + 1);
+            }
+            model.addRow(fila);
+        }
+
+    } catch (SQLException e) {
+        JOptionPane.showMessageDialog(null, "Error al cargar vehículos: " + e.getMessage());
+    }
+
+    jTableVehiculos.setModel(model);
 }
+
+}
+
